1b4326fbee5cf4daa00d51fdcc9194598e6c24a5
Luke Benstead <kazade@gmail.com>
2014-09-26 21:53:37 +0100

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= soil
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*/*.c)
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_CFLAGS	:= "-std=c99"
+include $(BUILD_SHARED_LIBRARY)
